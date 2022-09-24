fec5c2bc37f49897a124100edf5e314d1de4381c
leoarthurwall <leowall93@gmail.com>
2022-09-24 12:53:49 +0100

    aded value and and onChange handler to the input box

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import InputField from './components/InputField';
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("")
 
+  console.log(todo)
+
   return (
     <div className="App">
       <Header title='Hello World' color='white'/>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,25 @@
 import "./styles.css";
 
-interface Props{
-  todo: string; 
+interface Props {
+  todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const InputField: React.FC<Props> = ({todo, setTodo}) => {
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
-    <form className='input'>
-        <input className='input-box' type="input" placeholder="add a task..."/>
-        <button className='input-submit' type='submit'>
-            Add</button>
+    <form className="input">
+      <input
+        className="input-box"
+        type="input"
+        placeholder="add a task..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input-submit" type="submit">
+        Add
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
