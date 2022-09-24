98e0c3fd2207d3d0c293382a4ea8e6d1e1511456
leoarthurwall <leowall93@gmail.com>
2022-09-24 13:06:50 +0100

    created an a state for the todos with an initial state of an empty array. provided it the type Todo interface created in to model folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css';
-import { useState } from 'react';
-import Header from './components/Header';
-import InputField from './components/InputField';
-
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("")
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  console.log(todo)
+  console.log(todo);
 
   return (
     <div className="App">
-      <Header title='Hello World' color='white'/>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <Header title="Hello World" color="white" />
+      <InputField todo={todo} setTodo={setTodo} />
     </div>
   );
-}
+};
 
 export default App;
